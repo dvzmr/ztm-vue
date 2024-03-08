2ff1c771b0d99597a574c1bba1a97c1828fe606c
David Souza de Moura <mourazdavid@gmail.com>
2024-03-08 10:31:30 -0300

    first commit

---
+++ Section 2/section-2-1/app.js
@@ -0,0 +1,38 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            url: 'https://google.com',
+            raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+            age: 20
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+        increment() {
+            this.age++
+        },
+        updateLastName(msg, event) {
+
+            console.log(msg)
+
+            this.lastName = event.target.value
+        }
+    }
+}).mount('#app')
+
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 2000)
+
+// Vue.createApp({
+//     data() {
+//         return{
+//             firstName: 'Jane',
+//             lastName: 'Dought'
+//         }
+//     }
+// }).mount('#testapp')
